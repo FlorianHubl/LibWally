7256123ed3fe9c07fbc75e54eeee26e0fc53bdee
flohubl <flohubl@smail.hs-bremerhaven.de>
2023-10-08 22:15:52 +0200

    updated

--- Package.swift
+++ Package.swift
@@ -18,11 +18,6 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "LibWally",
-            dependencies: []),
-        .testTarget(
-            name: "LibWallyTests",
-            dependencies: ["LibWally"]),
+        .binaryTarget(name: "LibWally", path: "LibWally.xcframework")
     ]
 )
